7cdfed30019a702450aac61c8ff4973494d2e8fd
Jonatan Magnusson <jonatan.magnusson@above.se>
2021-04-08 15:32:41 +0200

    Chapter 3

--- exercises/ch-3-ex-1/client.js
+++ exercises/ch-3-ex-1/client.js
@@ -1,10 +1,10 @@
-var express = require("express");
-var request = require("sync-request");
-var url = require("url");
-var qs = require("qs");
+var express = require('express');
+var request = require('sync-request');
+var url = require('url');
+var qs = require('qs');
 var querystring = require('querystring');
 var cons = require('consolidate');
-var randomstring = require("randomstring");
+var randomstring = require('randomstring');
 var __ = require('underscore');
 __.string = require('underscore.string');
 
@@ -16,20 +16,19 @@ app.set('views', 'files/client');
 
 // authorization server information
 var authServer = {
-	authorizationEndpoint: 'http://localhost:9001/authorize',
-	tokenEndpoint: 'http://localhost:9001/token'
+  authorizationEndpoint: 'http://localhost:9001/authorize',
+  tokenEndpoint: 'http://localhost:9001/token',
 };
 
 // client information
-
 
 /*
  * Add the client information in here
  */
 var client = {
-	"client_id": "",
-	"client_secret": "",
-	"redirect_uris": ["http://localhost:9000/callback"]
+  client_id: 'oauth-client-1',
+  client_secret: 'oauth-client-secret-1',
+  redirect_uris: ['http://localhost:9000/callback'],
 };
 
 var protectedResource = 'http://localhost:9002/resource';
@@ -40,51 +39,124 @@ var access_token = null;
 var scope = null;
 
 app.get('/', function (req, res) {
-	res.render('index', {access_token: access_token, scope: scope});
+  res.render('index', { access_token: access_token, scope: scope });
 });
 
-app.get('/authorize', function(req, res){
+app.get('/authorize', function (req, res) {
+  // Clear the global access_token variable
+  access_token = null;
 
-	/*
-	 * Send the user to the authorization server
-	 */
-	
+  // Generate random state string and assign the global state variable
+  state = randomstring.generate();
+
+  var authorizeUrl = buildUrl(authServer.authorizationEndpoint, {
+    response_type: 'code',
+    client_id: client.client_id,
+    redirect_uri: client.redirect_uris[0],
+    state,
+  });
+
+  console.log(`Redirect to ${authorizeUrl}`);
+  res.redirect(authorizeUrl);
 });
 
-app.get('/callback', function(req, res){
+app.get('/callback', function (req, res) {
+  console.log('Incoming request: /callback');
 
-	/*
-	 * Parse the response from the authorization server and get a token
-	 */
-	
+  if (req.query.error) {
+    res.render('error', { error: req.query.error });
+    return;
+  }
+
+  if (req.query.state != state) {
+    console.log(
+      `State DOES NOT MATCH: expected ${state}, got ${req.query.state}`,
+    );
+    res.render('error', { error: 'State value did not match' });
+    return;
+  }
+
+  var code = req.query.code;
+  console.log(` - Code: ${code}`);
+
+  const form_data = qs.stringify({
+    grant_type: 'authorization_code',
+    code: code,
+    redirect_uri: client.redirect_uris[0],
+  });
+
+  const headers = {
+    'Content-Type': 'application/x-www-form-urlencoded',
+    Authorization: `Basic ${encodeClientCredentials(
+      client.client_id,
+      client.client_secret,
+    )}`,
+  };
+
+  const tokRes = request('POST', authServer.tokenEndpoint, {
+    body: form_data,
+    headers,
+  });
+
+  console.log(`Requesting access token for code ${code}`);
+
+  if (tokRes.statusCode >= 200 && tokRes.statusCode < 300) {
+    const body = JSON.parse(tokRes.getBody());
+
+    // Assign the global access_token variable
+    access_token = body.access_token;
+
+    console.log(`Got access token ${access_token}`);
+    res.render('index', { access_token, scope });
+  } else {
+    res.render('error', {
+      error: `Unable to fetch access token. Server response: ${tokRes.statusCode}`,
+    });
+  }
 });
 
-app.get('/fetch_resource', function(req, res) {
+app.get('/fetch_resource', function (req, res) {
+  if (!access_token) {
+    res.render('error', { error: 'Missing access token' });
+    return;
+  }
 
-	/*
-	 * Use the access token to call the resource server
-	 */
-	
+  const headers = {
+    Authorization: `Bearer ${access_token}`,
+  };
+
+  const resource = request('POST', protectedResource, { headers });
+
+  if (resource.statusCode >= 200 && resource.statusCode < 300) {
+    const body = JSON.parse(resource.getBody());
+    res.render('data', { resource: body });
+  } else {
+    res.render('error', {
+      error: `Server returned response code ${resource.statusCode}`,
+    });
+  }
 });
 
-var buildUrl = function(base, options, hash) {
-	var newUrl = url.parse(base, true);
-	delete newUrl.search;
-	if (!newUrl.query) {
-		newUrl.query = {};
-	}
-	__.each(options, function(value, key, list) {
-		newUrl.query[key] = value;
-	});
-	if (hash) {
-		newUrl.hash = hash;
-	}
-	
-	return url.format(newUrl);
+var buildUrl = function (base, options, hash) {
+  var newUrl = url.parse(base, true);
+  delete newUrl.search;
+  if (!newUrl.query) {
+    newUrl.query = {};
+  }
+  __.each(options, function (value, key, list) {
+    newUrl.query[key] = value;
+  });
+  if (hash) {
+    newUrl.hash = hash;
+  }
+
+  return url.format(newUrl);
 };
 
-var encodeClientCredentials = function(clientId, clientSecret) {
-	return Buffer.from(querystring.escape(clientId) + ':' + querystring.escape(clientSecret)).toString('base64');
+var encodeClientCredentials = function (clientId, clientSecret) {
+  return Buffer.from(
+    querystring.escape(clientId) + ':' + querystring.escape(clientSecret),
+  ).toString('base64');
 };
 
 app.use('/', express.static('files/client'));
@@ -94,4 +166,3 @@ var server = app.listen(9000, 'localhost', function () {
   var port = server.address().port;
   console.log('OAuth Client is listening at http://%s:%s', host, port);
 });
- 
